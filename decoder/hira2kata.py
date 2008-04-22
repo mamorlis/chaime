bf67e8d799cb999809a23416ac2cd0cc4e3a0732
tkng <tkng@users.noreply.github.com>
2008-04-22 17:31:50 +0000

    * hira2kata.py (hira2kata): Don't convert numeric and symbol to other character.  (katakana_value): New function.

--- decoder/hira2kata.py
+++ decoder/hira2kata.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 # encoding: utf-8
 
+def katakana_value(x):
+  x_num = ord(x)
+  if x_num >= 0x3041 and x_num <= 0x309F:
+    return unichr(ord(x) + 0x60)
+  else:
+    return x
+
 def hira2kata(hira):
   """Convert Hiragana and Katakana"""
-  kata = [ unichr(ord(x) + 0x60) for x in hira.decode("utf-8") ]
+  kata = [ katakana_value(x) for x in hira.decode("utf-8") ]
   return "".join(kata).encode("utf-8")
