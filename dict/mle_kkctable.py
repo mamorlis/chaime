cf0208fc8bba49d0336de5ac2ca50ee3cfc25a11
Mamoru Komachi <mamoru.komachi@gmail.com>
2008-06-03 13:17:29 +0000

    - ngram2kkm.py (main): Slurp Google N-gram's original format and use   MeCab's partial tagger to annotate readings. - mle_kkctable.py (main): Use Hiragana to store Kana-Kanji model.   This reduces search time during decoding, as hira2kata is called   too often according to profiling.

--- dict/mle_kkctable.py
+++ dict/mle_kkctable.py
@@ -2,28 +2,30 @@
 # Created on 09 Feb 2008 Mamoru Komachi <mamoru-k@is.naist.jp>
 
 import sys
-from optparse import OptionParser
+import math
+sys.path.append("../decoder")
+from hira2kata import kata2hira
 
-def main(kana, fp):
+scaling = 2 ** 12
+
+def main():
   words       = {}
   input_words = {}
-  for line in fp.readlines():
-    (word, read, freq) = line.split()
-    if kana:
-      (word, read) = (read, word)   # need kana-kanji table
+  for line in sys.stdin.readlines():
+    try:
+      (word, read, freq) = line.split()
+    except ValueError, e:
+      sys.stderr.write(line)
     if len(word) < 1 or len(read) < 1 or freq < 1:
       continue
     input = (word, read)
     input_words[input] = input_words.get(input, 0) + int(freq)
     words[word] = words.get(word, 0) + int(freq)
   for input in input_words.iteritems():
-    print input[0][0], input[0][1], \
-        float(input[1]) / float(words[input[0][0]])
+    (word, read, freq) = input[0][0], input[0][1], input[1]
+    prob = freq / float(words[input[0][0]])
+    cost = int(scaling * -math.log(float(prob)))
+    print "%s\t%s\t%s" % ( word, kata2hira(read), cost )
 
 if __name__ == "__main__":
-  parser = OptionParser()
-  parser.add_option("-k", "--kana", dest="kana", action="store_true",
-                    default=False,
-                    help="Contsruct Kana to Kanji table")
-  (opts, args) = parser.parse_args()
-  main(opts.kana, sys.stdin)
+  main()
--- dict/ngram2kkm.py
+++ dict/ngram2kkm.py
@@ -3,57 +3,41 @@
 # Created on 7 Apr 2008 Mamoru Komachi <mamoru-k@is.naist.jp>
 # ngram2kkm.py [list of ngram.gz]
 
-import gzip
-import pytc
+import MeCab
 import sys
 from optparse import OptionParser
 
 def main():
-  """Google N-gram Format in TSV
-word    read    count
-EOS
-  """
-  parser = OptionParser()
-  parser.add_option("-w", "--word", dest="word",
-                    help="Filename of a word dict (tcb)")
-  parser.add_option("-m", "--mle", dest="mle",
-                    help="Filename of a word dict with maximum likelihood estimation (tcb)")
-  (opts, args) = parser.parse_args()
-
-  #try:
-  #  words = pytc.BDB(opts.word, pytc.BDBOWRITER)
-  #except:
-  #  sys.stderr.write("Cannot open tcb:%s\n" % (opts.word))
-  #  sys.exit(-1)
-  words = {}
   try:
-    mle_words = pytc.BDB(opts.mle, pytc.BDBOWRITER)
-  except:
-    sys.stderr.write("Cannot open tcb:%s\n" % (opts.mle))
+    t = MeCab.Tagger("-p")
+  except RuntimeError, e:
+    sys.stderr.write("Cannot exec MeCab:%s\n" % (e))
     sys.exit(-1)
-
-  for f in args:
+  kkm_table = {}
+  for f in sys.argv[1:]:
     try:
-      fp = gzip.open(f, 'r')
-      for line in fp.readlines():
-        (word, read, freq) = line.rstrip().split("\t")
-        words[word] = words.get(word, '') + read + "\t" + freq + "\n"
+      fp = open(f, 'r')
+      for line in fp.xreadlines():
+        line = line.rstrip()
+        (ngram, freq) = line.split("\t")
+        partial = [ x + "\t*" for x in ngram.split(" ") ]
+        partial.append("EOS\n")
+        m = t.parseToNode("\n".join(partial))
+        while m:
+          try:
+            reading = m.feature.split(",")[7]
+            if m.surface != '' and reading != '*':
+              kkm_table[(m.surface, reading)] = freq
+          except IndexError:
+            # mecab doesn't output to some words
+            pass
+          m = m.next
       sys.stderr.write("Read %s\n" % (f))
     except IOError, e:
       sys.stderr.write("Cannot open file %s:%s" % (f, e))
       sys.exit(-1)
-  for (word, keys) in words.iteritems():
-    best_read = ''
-    best_freq = 0
-    total_freq = 0
-    for read_freq in keys.split("\n"):
-      (read, freq) = read_freq.split("\t")
-      if best_freq < int(freq):
-        best_freq = int(freq)
-        best_read = read
-      total_freq += int(freq)
-    mle_words[word] = "%s %s" % (best_read, float(best_freq) / total_freq)
-  mle_words.close()
+  for (word, keys) in kkm_table.iteritems():
+    print "%s\t%s\t%s" % (word[0], word[1], keys)
 
 if __name__ == "__main__":
   main()
